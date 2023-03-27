547e55133985484cb8d7b551d5dfffe4611a622c
Walter Jaworski <walter.jaworski@gmail.com>
2023-03-27 13:46:44 -0300

    first commit

---
+++ api/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('express-async-errors');
+
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
+
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));

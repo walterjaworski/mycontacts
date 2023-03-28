19e4b5e59147c13ae656f7374b0dd66a3778c317
Walter Jaworski <walter.jaworski@gmail.com>
2023-03-28 18:06:53 -0300

    begin app with editorconfig, eslint and packages

---
+++ fe/src/pages/Home/index.js
@@ -0,0 +1,93 @@
+import {
+  Container, Header, ListContainer, Card, InputSearchContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+export default function Home() {
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato" />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>
+              Nome
+            </span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Walter Jaworski</strong>
+              <small>instagram</small>
+            </div>
+            <span>walter@gmail.com</span>
+            <span>(41) 9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Walter Jaworski</strong>
+              <small>instagram</small>
+            </div>
+            <span>walter@gmail.com</span>
+            <span>(41) 9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Walter Jaworski</strong>
+              <small>instagram</small>
+            </div>
+            <span>walter@gmail.com</span>
+            <span>(41) 9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
+}
---
+++ fe/src/pages/NewContact/index.js
@@ -0,0 +1,9 @@
+import PageHeader from '../../components/PageHeader';
+
+export default function NewContact() {
+  return (
+    <PageHeader
+      title="Novo Contato"
+    />
+  );
+}

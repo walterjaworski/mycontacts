dc60462e92e31894ce5b6026928359f14c099bbb
Walter Jaworski <walter.jaworski@gmail.com>
2023-04-03 09:56:43 -0300

    better error code

--- fe/src/services/utils/HttpClient.js
+++ fe/src/services/utils/HttpClient.js
@@ -6,14 +6,23 @@ class HttpClient {
   }
 
   async get(path) {
-    const response = await fetch(`${this.baseURL}${path}`);
     await delay(500);
 
-    if (response.ok) {
-      return response.json();
+    const response = await fetch(`${this.baseURL}${path}`);
+
+    let body = null;
+    const contentType = response.headers.get('Content-Type');
+    if (contentType.includes('application/json')) {
+      body = await response.json();
     }
 
-    throw new Error(`${response.status} - ${response.statusText}`);
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(
+      body?.error || `${response.status} - ${response.statusText}`,
+    );
   }
 }
 

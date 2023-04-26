3d0fef18fa025a6d480ee17fabdf263b18760f57
Walter Jaworski <walter.jaworski@gmail.com>
2023-04-26 11:23:34 -0300

    add custom event manager

--- fe/src/lib/EventManager.js
+++ fe/src/lib/EventManager.js
@@ -1,27 +1,29 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {};
+    this.listeners = new Map();
   }
 
-  on(event, listeners) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+  on(event, listener) {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
-    this.listeners[event].push(listeners);
+
+    this.listeners.get(event).push(listener);
   }
 
   emit(event, payload) {
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
 
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload);
     });
   }
 
   removeListener(event, listenerToRemove) {
-    const listeners = this.listeners[event];
+    const listeners = this.listeners.get(event);
+
     if (!listeners) {
       return;
     }
@@ -30,23 +32,6 @@ export default class EventManager {
       (listener) => listener !== listenerToRemove,
     );
 
-    this.listeners[event] = filteredListeners;
+    this.listeners.set(event, filteredListeners);
   }
 }
-
-const toastEventManager = new EventManager();
-
-function addToast1(payload) {
-  console.log('addtoast listener 1', payload);
-}
-function addToast2(payload) {
-  console.log('addtoast listener 2', payload);
-}
-
-toastEventManager.on('addtoast', addToast1);
-toastEventManager.on('addtoast', addToast2);
-toastEventManager.emit('addtoast', { type: 'danger', text: 'texto' });
-
-toastEventManager.removeListener('addtoast', addToast1);
-toastEventManager.emit('addtoast', 'depois de remover');
-console.log(toastEventManager);

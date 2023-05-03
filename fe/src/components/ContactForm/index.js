6a226c3d38fb87a2dda6751c2dbaeaa75b93c082
Walter Jaworski <walter.jaworski@gmail.com>
2023-05-03 10:43:07 -0300

    create custom hook to resolve state warning

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -17,13 +17,15 @@ import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
+import useSafeAsyncState from '../../hooks/useSafeAsyncState';
+
 export const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [categoryId, setCategoryId] = useState('');
-  const [categories, setCategories] = useState([]);
-  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
+  const [categories, setCategories] = useSafeAsyncState([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useSafeAsyncState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const {
@@ -61,7 +63,7 @@ export const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
     }
 
     loadCategories();
-  }, []);
+  }, [setCategories, setIsLoadingCategories]);
 
   function handleNameChange(event) {
     setName(event.target.value);
